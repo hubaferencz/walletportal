99016d1f991d09b8a83723120be3fe6e25b1a931
Huba Ferencz <huba.ferencz.personal@gmail.com>
2023-02-14 17:43:23 +0200

    affiliate changes

--- src/pages/affiliate/affiliateData.js
+++ src/pages/affiliate/affiliateData.js
@@ -1,13 +1,9 @@
 // array of items for affiliate page and export it
 import binance from './logos/binance.png'
-import revolut from './logos/revolut.png'
+
 import bybit from './logos/bybit.png'
 import coinbase from './logos/coinbase.png'
-import gemini from './logos/gemini.png'
-import okx from './logos/okx.png'
 import swissborg from './logos/swissborg.png'
-import cryptocom from './logos/cryptocom.png'
-
 
 
 const affiliateData = [
@@ -20,26 +16,11 @@ const affiliateData = [
         description: 'Buy crypto from Switzerland and get up to to €100 in CHSB.',
     },
     {
-        id: 221,
-        title: 'Revolut',
-        link: 'https://revolut.com/referral/hubaylhm8!FEB1-23-AR',
-        image: revolut,
-        description: 'Buy cryptocurrencies directly from your Revolut online bank account.',
-    },
-   
-    {
         id: 3,
         title: 'Coinbase',
         link: 'https://coinbase.com/join/MR8IEQ?src=referral-link',
         image: coinbase,
         description: 'Register at Coinbase and get $10 guaranteed in Bitcoin for free.',
-    },
-    {
-        id: 4,
-        title: 'Gemini',
-        link: 'https://gemini.com/share/ryr44ykh6',
-        image: gemini,
-        description: 'Buy crypto using this beautiful interface and get $10 in Bitcoin for free.',
     },
     {
         id: 1,
@@ -54,21 +35,6 @@ const affiliateData = [
         link: 'https://www.bybit.com/invite?ref=O8RPOA',
         image: bybit,
         description: 'Buy crypto with 0% fees and get up to 5,030 USDT in bonuses.',
-    },
-    {
-        id: 5,
-        title: 'OKX',
-        link: 'https://www.okx.com/join/93408492',
-        image: okx,
-        description: 'Sign up and log in OKX App to claim a Mystery Box up to $10,000',
-    },
-    
-    {
-        id: 7,
-        title: 'Crypto.com',
-        link: 'https://crypto.com/app/3c5nmeafj7',
-        image: cryptocom,
-        description: 'Join Crypto.com and get $25 in CRO for free at registration.',
     },
 
 ]
